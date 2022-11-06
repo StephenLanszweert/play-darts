0a57c4d8146b2ba215ab12a15deb338df3ab0689
Stephen Lanszweert <stephenlanszweert@hotmail.be>
2022-11-06 20:30:03 +0100

    Finish setup dashboard screen and manifest for PWA

--- libs/modules/dashboard/src/lib/dashboard.module.ts
+++ libs/modules/dashboard/src/lib/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 import * as fromPages from './pages';
 import { DashboardRoutingModule } from './dashboard.routing.module';
+import { Chart } from 'chart.js';
 
 @NgModule({
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, ChartModule, DashboardRoutingModule],
   declarations: [fromPages.pages]
 })
 export class DashboardModule { }
--- libs/modules/dashboard/src/lib/pages/dashboard/dashboard.component.ts
+++ libs/modules/dashboard/src/lib/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'playdarts-dashboard',
   styleUrls: ['./dashboard.component.scss'],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+  basicData: any;
+  basicOptions: any;
+  mobile: boolean = false;
+
+  ngOnInit(): void {
+    Chart.register(ChartDataLabels);
+    if (window.screen.width <= 850) {
+      this.mobile = true;
+    }
+    this.basicData = {
+      labels: ['05/05/22', '10/05/22', '15/5/22', '20/5/22', '25/5/22'],
+      datasets: [
+        {
+          label: 'Avg',
+          data: [65.23, 59.25, 80.00, 81.15, 56.45],
+          fill: false,
+          borderColor: '#F26C6D',
+          tension: .4
+        }
+      ]
+    };
+    this.basicOptions = {
+      defaults: {
+        font: {
+          family: "'Poppins', sans-serif"
+        }
+      },
+      // interaction: {
+      //   mode: 'nearest',
+      //   axis: 'x',
+      //   intersect: false
+      // },
+      responsive: true,
+      plugins: {
+        datalabels: {
+          backgroundColor: '#050A0C',
+          borderRadius: 20,
+          color: 'white',
+          font: {
+            weight: 'bold'
+          },
+          padding: 6
+        },
+        legend: {
+          display: false,
+          labels: {
+            color: '#495057'
+          }
+        },
+        tooltip: {
+          enabled: false
+        },
+        point: {
+          pointStyle: 'cross'
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        }
+      }
+    };
+  }
+}
